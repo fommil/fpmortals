6b768097cd3662f1abd2df393f9c7e503c44900a
Sam Halliday <sam.halliday@gmail.com>
2017-02-18 14:56:58 +0000

    basic algebra

--- src/main/scala/algebra.scala
+++ src/main/scala/algebra.scala
@@ -1,2 +1,34 @@
 // Copyright: 2017 https://github.com/fommil/drone-dynamic-agents/graphs
 // License: http://www.apache.org/licenses/LICENSE-2.0
+package algebra
+
+import java.util.UUID
+
+object Timing {
+  sealed trait Ops[A]
+  case class GetTime() extends Ops[String]
+}
+
+object Drone {
+  sealed trait Ops[A]
+  case class ReceiveWorkQueue() extends Ops[Int] // will be push
+  case class ReceiveActiveWork() extends Ops[Int] // will be push
+}
+
+object Container {
+  sealed trait Ops[A]
+  case class GetTime() extends Ops[String]
+  case class GetNodes() extends Ops[List[String]]
+  case class StartAgent() extends Ops[UUID]
+  case class StopAgent(uuid: UUID) extends Ops[Unit]
+
+  case class ReceiveKillEvent() extends Ops[UUID] // will be push
+}
+
+object Audit {
+  case class Auditable()
+
+  sealed trait Ops[A]
+  case class StoreEvent(e: Auditable) extends Ops[Unit]
+}
+
