09e1c6903fabbeed615859951c18734a9837dd0f
Sam Halliday <sam.halliday@gmail.com>
2017-03-27 16:22:21 +0100

    refine the return type

--- src/main/scala/logic.scala
+++ src/main/scala/logic.scala
@@ -5,7 +5,7 @@ package logic
 import algebra._
 import cats.implicits._
 import freestyle.implicits._
-import freestyle._
+import freestyle.FreeS
 import Machines._
 
 /**
@@ -38,13 +38,16 @@ class DynamicAgents[F[_]](
       case (w, a, av, ac) => State(w.items, a.items, av.nodes, ac.nodes, Set.empty)
     }
 
-  def act(state: State) = state match {
+  def act(state: State): FreeS[F, State] = state match {
     // when there is pending work, but no agents or pending nodes, start a node
     case State(w, 0, managed, active, pending) if w > 0 && active.isEmpty && pending.isEmpty =>
       // note there is a timing issue here, we might want to give some
       // grace time for a pending node to become an agent.
       val starting = managed.head
-      for { started <- c.start(starting) } yield (started, state.copy(pending = Set(starting)))
+      for {
+        _ <- c.start(starting)
+        update = state.copy(pending = Set(starting))
+      } yield update
 
     // when there is no pending work, stop all active nodes
     case State(0, _, managed, active, pending) if active.nonEmpty =>
