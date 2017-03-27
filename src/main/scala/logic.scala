38d943687e53c3c2a58b7b094846231c742c3508
Sam Halliday <sam.halliday@gmail.com>
2017-03-27 17:22:10 +0100

    compiling

--- src/main/scala/logic.scala
+++ src/main/scala/logic.scala
@@ -52,10 +52,8 @@ class DynamicAgents[F[_]](
     // when there is no pending work, stop all active nodes
     case State(0, _, managed, active, pending) if active.nonEmpty =>
       val stopping: List[FreeS[F, Node]] = (active -- pending).toList.map { n => c.stop(n).map(_ => n) }
-
-      // TODO: go over the Nodes and update the state by adding to pending
-      // stopping.sequenceU.fold(state) { ... }
-      ???
+      val sequenced: FreeS[F, List[Node]] = stopping.sequenceU
+      sequenced.map { nodes => nodes.foldLeft(state) { (state, node) => state.copy(pending = state.pending + node) } }
 
     // do nothing...
     case _ => FreeS.pure(state)
