9a40ef5a3f31cfa60c2a03c5fdc3181ac1139b79
Sam Halliday <sam.halliday@gmail.com>
2017-03-28 21:12:13 +0100

    Revert State experiment
    This reverts commit c02ecfcb8dea357a2db02d07aeb76860f4a8a71b.

--- src/main/scala/logic.scala
+++ src/main/scala/logic.scala
@@ -12,7 +12,6 @@ import cats.data.{NonEmptyList => Nel}
 import cats.implicits._
 import freestyle._
 import freestyle.implicits._
-import freestyle.effects.state
 
 /**
  * @param backlog how many builds are waiting to be run on the ci
@@ -44,55 +43,47 @@ object coproductk {
     val d: Drone[F]
     val c: Machines[F]
   }
-  val st = state[WorldView]
 }
 import coproductk._
 
 final case class DynAgentsLogic[F[_]](
   implicit
-  m: DynAgents[F],
-  s: st.StateM[F]
+  m: DynAgents[F]
 ) {
   import m._
 
-  def initial: FreeS[F, Unit] =
+  def initial: FreeS[F, WorldView] =
     (d.getBacklog |@| d.getAgents |@| c.getManaged |@| c.getAlive |@| c.getTime).map {
-      case (w, a, av, ac, t) =>
-        s.set(
-          WorldView(w.items, a.items, av.nodes, ac.nodes, Map.empty, t.time)
-        )
+      case (w, a, av, ac, t) => WorldView(w.items, a.items, av.nodes, ac.nodes, Map.empty, t.time)
     }
 
-  def act: FreeS[F, Unit] =
-    for {
-      state <- s.get
-      _ <- state match {
-        // when there is a backlog, but no agents or pending nodes, start a node
-        case WorldView(w, 0, Nel(start, _), alive, pending, time) if w > 0 && alive.isEmpty && pending.isEmpty =>
-          for {
-            _ <- c.start(start)
-            _ <- s.modify(_.copy(pending = Map(start -> time)))
-          } yield ()
+  def act(state: WorldView): FreeS[F, WorldView] = state match {
+    // when there is a backlog, but no agents or pending nodes, start a node
+    case WorldView(w, 0, Nel(start, _), alive, pending, time) if w > 0 && alive.isEmpty && pending.isEmpty =>
+      for {
+        _ <- c.start(start)
+        update = state.copy(pending = Map(start -> time))
+      } yield update
 
-        // when there is no pending work, stop all alive nodes. However,
-        // since Google / AWS charge per hour we only shut down machines
-        // in their 58th+ minute. Assumes that we are called fairly
-        // regularly (otherwise we may miss this window). Also a safety
-        // cap of ~5 hours for any node.
-        case WorldView(0, _, managed, alive, pending, time) if alive.nonEmpty =>
-          (alive -- pending.keys).toList
-            .flatMap {
-              case (n, started) =>
-                val up = ChronoUnit.MINUTES.between(started, time)
-                if (up % 60 >= 58 || up >= 290) Some(n)
-                else None
-            }.traverse { n => c.stop(n).map(_ => n) }
-            .map { nodes =>
-              nodes.foldLeft(state) { (state, node) => state.copy(pending = state.pending + (node -> time)) }
-            }
+    // when there is no pending work, stop all alive nodes. However,
+    // since Google / AWS charge per hour we only shut down machines
+    // in their 58th+ minute. Assumes that we are called fairly
+    // regularly (otherwise we may miss this window). Also a safety
+    // cap of ~5 hours for any node.
+    case WorldView(0, _, managed, alive, pending, time) if alive.nonEmpty =>
+      (alive -- pending.keys).toList
+        .flatMap {
+          case (n, started) =>
+            val up = ChronoUnit.MINUTES.between(started, time)
+            if (up % 60 >= 58 || up >= 290) Some(n)
+            else None
+        }.traverse { n => c.stop(n).map(_ => n) }
+        .map { nodes =>
+          nodes.foldLeft(state) { (state, node) => state.copy(pending = state.pending + (node -> time)) }
+        }
 
-        // do nothing...
-        case _ => FreeS.pure(state)
-      }
-    } yield ()
+    // do nothing...
+    case _ => FreeS.pure(state)
+  }
+
 }
