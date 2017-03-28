de6e03fc43991c33b8d416ece2fe6e4c3a014ef2
Sam Halliday <sam.halliday@gmail.com>
2017-03-28 12:04:22 +0100

    add boilerplate for a test

---
+++ src/test/scala/LogicSpec.scala
@@ -0,0 +1,45 @@
+// Copyright (C) 2017 Sam Halliday
+// License: http://www.gnu.org/licenses/gpl.html
+
+import org.scalatest._
+import org.scalatest.Matchers._
+
+import cats._
+
+import algebra._
+import logic._
+
+object IdInterpreters {
+
+  implicit def drone: Drone.Services.Op ~> Id = new (Drone.Services.Op ~> Id) {
+    def apply[A](fa: Drone.Services.Op[A]): Id[A] = fa match {
+      case _ => ???
+    }
+  }
+
+  implicit def machines: Machines.Services.Op ~> Id = new (Machines.Services.Op ~> Id) {
+    def apply[A](fa: Machines.Services.Op[A]): Id[A] = fa match {
+      case _ => ???
+    }
+  }
+
+  implicit def audit: Audit.Services.Op ~> Id = new (Audit.Services.Op ~> Id) {
+    def apply[A](fa: Audit.Services.Op[A]): Id[A] = fa match {
+      case _ => ???
+    }
+  }
+
+  //val impl = new DynamicAgents
+}
+
+class LogicSpec extends FlatSpec {
+
+  "Business Logic" should "generate an initial state" in {
+    import IdInterpreters._
+
+
+//    impl.initial
+
+  }
+
+}
