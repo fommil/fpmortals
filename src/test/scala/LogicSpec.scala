171551318b1ca1645f8576ba248b42bab2066720
Sam Halliday <sam.halliday@gmail.com>
2017-03-28 17:51:27 +0100

    compiling

--- src/test/scala/LogicSpec.scala
+++ src/test/scala/LogicSpec.scala
@@ -1,42 +1,46 @@
-// Copyright (C) 2017 Sam Halliday
-// License: http://www.gnu.org/licenses/gpl.html
+// Copyright: 2017 https://github.com/fommil/drone-dynamic-agents/graphs
+// License: http://www.apache.org/licenses/LICENSE-2.0
+package tests
 
+import algebra._
+import cats._
+import freestyle.implicits._
+import logic._
 import org.scalatest._
 import org.scalatest.Matchers._
 
-import cats._
-import cats.free._
-
-import algebra._
-import logic._
-
-import freestyle.implicits._
-
 object IdInterpreters {
 
-  implicit def drone = new Drone.Services.Handler[Id] { }
+  implicit val drone: Drone.Services.Handler[Id] = new Drone.Services.Handler[Id] {
+    import algebra.Drone._
+    def getActiveWork: Id[WorkActive] = ???
+    def getWorkQueue: Id[WorkQueue] = ???
+  }
 
-  implicit def machines = new Machines.Services.Handler[Id] { }
+  implicit val machines: Machines.Services.Handler[Id] = new Machines.Services.Handler[Id] {
+    import algebra.Machines._
+    def getAlive: Id[Alive] = ???
+    def getManaged: Id[Managed] = ???
+    def getTime: Id[Time] = ???
+    def start(node: Node): Id[Node] = ???
+    def stop(node: Node): Id[Unit] = ???
+  }
 
-  implicit def audit = new Audit.Services.Handler[Id] {  }
+  implicit val audit: Audit.Services.Handler[Id] = new Audit.Services.Handler[Id] {
+    //import Audit._
+    def store(msg: String): Id[Unit] = ???
+  }
 
-//  import Modules.Services._
-//  import Modules.DynamicDroneDeps._
-
-//  import Drone.Services._
-//  import Machines.Services._
-//  import Audit.Services._
-
-  val impl = new DynamicAgents[Id]
+  // WTF? Why does this take Op instead of Id?
+  val impl = new DynamicAgents[Modules.Services.Op]
 }
 
 class LogicSpec extends FlatSpec {
 
   "Business Logic" should "generate an initial state" in {
-    import IdInterpreters._
+    //import IdInterpreters._
 
-
-//    impl.initial
+    //    impl.initial
 
   }
 
