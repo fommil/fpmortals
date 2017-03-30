e61112bda519a72155467c9681fe47c95c77c7e4
Sam Halliday <sam.halliday@gmail.com>
2017-03-30 22:34:28 +0100

    api investigations

--- src/main/scala/interpreters/dronefs2.scala
+++ src/main/scala/interpreters/dronefs2.scala
@@ -7,12 +7,15 @@ import io.circe.fs2._
 import spinoco.fs2.http
 import spinoco.fs2.http._
 import spinoco.protocol.http._
+import spinoco.protocol.http.header.Authorization
+import spinoco.protocol.http.header.value.HttpCredentials._
 
 import algebra.drone._
 
 final case class DroneConfig(
   host: String,
-  token: String
+  token: String,
+
 )
 
 import java.nio.channels.AsynchronousChannelGroup
@@ -32,8 +35,17 @@ final class DroneFs2(config: DroneConfig) extends Drone.Handler[Task] {
   // TODO: take clientTask as input so we can mock
   private val clientTask: Task[HttpClient[Task]] = http.client[Task]()
 
+  // doesn't quite tell us what we want...
+  // http://readme.drone.io/api/build-endpoint/
+  // https://github.com/drone/drone/blob/master/router/router.go#L166
+
+  // FIXME: investigate /api/info/queue
+
   // FIXME: we don't read Backlog off the wire, we read a custom format and then convert into Backlog
-  private val backlogRequest = HttpRequest.get[Task](Uri.https(config.host, "TODO"))
+  private val backlogRequest =
+    HttpRequest.get[Task](Uri.https(config.host, "/api/builds"))
+    .withHeader(Authorization(OAuth2BearerToken(config.token)))
+
   def getBacklog: Task[Backlog] = {
     clientTask.flatMap { client =>
       client.request(backlogRequest).flatMap { resp =>
