77191eefb6e88c236981fcee7404676ad3600c2b
Sam Halliday <sam.halliday@gmail.com>
2018-01-09 16:47:56 +0000

    KV basics

--- src/main/scala/const.scala
+++ src/main/scala/const.scala
@@ -6,21 +6,54 @@ package const
 import scalaz._, Scalaz._
 
 trait KV[F[_]] {
-  def get(key: String): F[Option[String]]
-  def put(key: String, a: String): F[Unit]
+  def read(key: String): F[Maybe[String]]
 }
 object KV {
   @inline def apply[F[_]: KV](implicit i: KV[F]): KV[F] = i
+
+  implicit val id: KV[Id] = new KV[Id] {
+    private val canned: IMap[String, String] = IMap("foo" -> "FOO")
+
+    def read(key: String): Maybe[String] = {
+      println(s"read $key")
+      canned.lookup(key).toMaybe
+    }
+  }
+
+  implicit val const: KV[Const[ISet[String], ?]] =
+    new KV[Const[ISet[String], ?]] {
+      def read(key: String) = Const(ISet.singleton(key))
+    }
+
 }
 
 object Program {
 
-   def program[F[_]: Apply: KV](): F[List[String]] =
-    (F.get("Cats"), F.get("Dogs"), F.put("Mice", "42"), F.get("Cats"))
-      .mapN((f, s, _, t) => List(f, s, t).flatten)
+  // TODO: need a more compelling "program" (this is just batch get... do
+  // something, maybe with another step like pinging some other algebra when we
+  // see things containing a substring)
+  def program[F[_]: Applicative: KV](
+    keys: IList[String]
+  ): F[IList[(String, String)]] =
+    keys
+      .traverse(k => KV[F].read(k).map(_.strengthL(k).toIList))
+      .map(_.join)
+
+  def optimised[F[_]: Applicative: KV](
+    keys: IList[String]
+  ): F[IList[(String, String)]] = {
+    val deduped = program[Const[ISet[String], ?]](keys).getConst
+    // println(deduped.toIList)
+
+    // TODO: use a batched version of KV
+    //deduped.toIList.traverse(KV[F].read)
+    program[F](deduped.toIList)
+  }
 
   def main(args: Array[String]): Unit = {
-
+    val query = IList("foo", "bar", "foo")
+    println(program[Id](query))   // [(foo,FOO),(foo,FOO)]
+    println(optimised[Id](query)) // [(foo,FOO)] FIXME FIXME FIXME
   }
 
 }
