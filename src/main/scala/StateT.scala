2a85cfa60e12344546fc7aa37fb12d1c74756719
Sam Halliday <sam.halliday@gmail.com>
2018-03-27 20:29:26 +0100

    statet changes

--- src/main/scala/StateT.scala
+++ src/main/scala/StateT.scala
@@ -9,12 +9,12 @@ import scalaz.syntax.monad._
 sealed abstract class StateT[F[_], S, A] {
   import StateT._
 
-  def run(initial: S)(implicit F: Monad[F]): F[(S, A)] = this match {
+  final def run(initial: S)(implicit F: Monad[F]): F[(S, A)] = this match {
     case Point(f) => f(initial)
     case FlatMap(Point(f), g) =>
-      f(initial) >>= { case (s, x) => g(s, x).run(s) }
-    case FlatMap(a @ FlatMap(f, g), h) =>
-      FlatMap(f, (s: S, x: a.Out) => FlatMap(g(s, x), h)).run(initial)
+      f(initial) >>= { case (s, a) => g(s, a).run(s) }
+    case FlatMap(a @ FlatMap(b, g), h) =>
+      FlatMap(b, (s: S, x: a.Out) => FlatMap(g(s, x), h)).run(initial)
   }
 
 }
