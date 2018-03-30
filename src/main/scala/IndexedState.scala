fad58335ca2fe63dd74282cd51633a8c0111167f
Sam Halliday <sam.halliday@gmail.com>
2018-03-30 23:19:52 +0100

    fixup! indexed state

--- src/main/scala/IndexedState.scala
+++ src/main/scala/IndexedState.scala
@@ -11,27 +11,24 @@ import Cache._
 trait Cache[M[_], K, V] {
   type F[in, out, a] = IndexedStateT[M, in, out, a]
 
-  // FIXME: types
+  def read[S <: Ready](k: K): F[S, S, Maybe[V]]
 
-  def view[I <: Ready, O <: Ready](k: K): F[I, O, Maybe[V]]
-  def lock[I <: Ready, O <: Locked]: F[I, O, Unit]
-  def update[I <: Locked, O <: Updated](k: K, v: V): F[I, O, Unit]
-  def commit[I <: Locked, O <: Ready]: F[I, O, Unit]
+  def lock: F[Ready, Locked, Unit]
+  def update(k: K, v: V): F[Locked, Updated, Unit]
+  def commit: F[Updated, Ready, Unit]
 }
-// can we use tags instead?
 object Cache {
-  // I'm not exactly happy that we need to use the scala language this way...
   sealed class Ready
-  class Locked extends Ready
-  class Updated extends Locked
+  sealed class Locked extends Ready
+  final class Updated extends Locked
 }
 
 object Main {
 
-  def foo[M[_]: Monad](C: Cache[M, Int, String]) =
+  def wibbleise[M[_]: Monad](C: Cache[M, Int, String]) =
     for {
       _ <- C.lock
-      a1 <- C.view(13)
+      a1 <- C.read(13)
       a2 = a1.cata(_ + "'", "wibble")
       _ <- C.update(13, a2)
       _ <- C.commit
