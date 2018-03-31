74a586e194437ae6fb2b670ed2a4c6840f01fbdc
Sam Halliday <sam.halliday@gmail.com>
2018-03-31 13:16:51 +0100

    Nothing

--- src/main/scala/IndexedState.scala
+++ src/main/scala/IndexedState.scala
@@ -12,6 +12,10 @@ trait Cache[M[_]] {
   type F[in, out, a] = IndexedStateT[M, in, out, a]
 
   def read[S <: Status](k: Int): F[S, S, Maybe[String]]
+  //def read[S <: Status](k: Int)(implicit NN: NotNothing[S]): F[S, S, Maybe[String]]
+  // def read(k: Int): F[Ready, Ready, Maybe[String]]
+  // def readLocked(k: Int): F[Locked, Locked, Maybe[String]]
+  // def readUncommitted(k: Int): F[Updated, Updated, Maybe[String]]
 
   def lock: F[Ready, Locked, Unit]
   def update(k: Int, v: String): F[Locked, Updated, Unit]
@@ -26,7 +30,7 @@ object Cache {
 
 object Main {
 
-  def wibbleise[M[_]: Monad](C: Cache[M]) =
+  def wibbleise[M[_]: Monad](C: Cache[M]): IndexedStateT[M, Ready, Ready, String] =
     for {
       _  <- C.lock
       a1 <- C.read(13)
@@ -34,4 +38,11 @@ object Main {
       _  <- C.update(13, a2)
       _  <- C.commit
     } yield a2
+
+  def fail[M[_]: Monad](C: Cache[M]): IndexedStateT[M, Locked, Ready, Maybe[String]] =
+    for {
+      a1 <- C.read(13)
+      _  <- C.update(13, "wibble")
+      _  <- C.commit
+    } yield a1
 }
