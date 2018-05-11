bb42fbce747e719b05240c110fc3374420c68825
Sam Halliday <sam.halliday@gmail.com>
2018-05-11 17:44:20 +0100

    trying to do it without var

--- src/test/scala/AlgebraSpec.scala
+++ src/test/scala/AlgebraSpec.scala
@@ -4,7 +4,7 @@
 package fommil
 package algebra
 
-import prelude._, Z._
+import prelude._, Z._, S._
 
 import org.scalatest._
 import org.scalatest.Matchers._
@@ -47,13 +47,16 @@ object Monitor extends (Demo.Ast ~> Demo.Ast) {
 
 trait BatchMachines[F[_]] {
   def start(nodes: NonEmptyList[MachineNode]): F[Unit]
+  def noop(): F[Unit]
 }
 object BatchMachines {
   sealed abstract class Ast[A]
   final case class Start(nodes: NonEmptyList[MachineNode]) extends Ast[Unit]
+  final case class Noop()                                  extends Ast[Unit]
   def liftF[F[_]](implicit I: Ast :<: F): BatchMachines[Free[F, ?]] =
     new BatchMachines[Free[F, ?]] {
       def start(nodes: NonEmptyList[MachineNode]) = Free.liftF(I(Start(nodes)))
+      def noop()                                  = Free.liftF(I(Noop()))
     }
 }
 
@@ -105,7 +108,7 @@ class AlgebraSpec extends FlatSpec {
       .shouldBe(1)
   }
 
-  it should "support monkey patching" in {
+  it should "support monkey patching part 1" in {
     type S = Set[MachineNode]
     val M = Mocker.stubAny[Machines.Ast, State[S, ?]] {
       case Machines.Stop(node) => State.modify[S](_ + node)
@@ -126,8 +129,125 @@ class AlgebraSpec extends FlatSpec {
       .shouldBe((Set(MachineNode("#tea")), ()))
   }
 
-  it should "support scary optimisations" in {
-    fail
+  it should "support monkey patching part 2" in {
+    case class S(
+      singles: IList[MachineNode],
+      batches: IList[NonEmptyList[MachineNode]]
+    ) {
+      def addSingle(node: MachineNode): S =
+        S(node :: singles, batches)
+      def addBatch(nodes: NonEmptyList[MachineNode]): S =
+        S(singles, nodes :: batches)
+    }
+
+    type T[a] = State[S, a]
+
+    type Orig[a]    = Coproduct[Machines.Ast, Drone.Ast, a]
+    type Patched[a] = Coproduct[BatchMachines.Ast, Orig, a]
+
+    val M: Machines.Ast ~> T = Mocker.stub[Unit] {
+      case Machines.Start(node) => State.modify[S](_.addSingle(node))
+    }
+    val D: Drone.Ast ~> T = Mocker.stub[Int] {
+      case Drone.GetBacklog() => 2.pure[T]
+    }
+    val B: BatchMachines.Ast ~> T = Mocker.stub[Unit] {
+      case BatchMachines.Start(nodes) => State.modify[S](_.addBatch(nodes))
+      case BatchMachines.Noop()       => ().pure[T]
+    }
+
+    def program[F[_]: Monad](M: Machines[F], D: Drone[F]): F[Unit] =
+      for {
+        todo <- D.getBacklog
+        _    <- (1 |-> todo).traverse(id => M.start(MachineNode(id.shows)))
+      } yield ()
+
+    program(Machines.liftF[Orig], Drone.liftF[Orig])
+      .foldMap(or(M, D))
+      .run(S(IList.empty, IList.empty))
+      ._1
+      .shouldBe(S(IList(MachineNode("2"), MachineNode("1")), IList.empty))
+
+    val interceptor: Orig ~> Patched = new (Orig ~> Patched) {
+      var saved: Maybe[MachineNode] = Maybe.empty
+
+      def apply[α](fa: Orig[α]): Patched[α] = fa.run match {
+        case -\/(Machines.Start(node)) =>
+          saved match {
+            case Maybe.Just(also) =>
+              saved = Maybe.empty
+              Coproduct.leftc(BatchMachines.Start(NonEmptyList(node, also)))
+            case Maybe.Empty() =>
+              saved = Maybe.just(node)
+              Coproduct.leftc(BatchMachines.Noop())
+          }
+
+        case other => Coproduct.rightc(Coproduct(other))
+      }
+    }
+
+    program(Machines.liftF[Orig], Drone.liftF[Orig])
+      .mapSuspension(interceptor)
+      .foldMap(or(B, or(M, D)))
+      .run(S(IList.empty, IList.empty))
+      ._1
+      .shouldBe(
+        S(
+          IList.empty,
+          IList(NonEmptyList(MachineNode("2"), MachineNode("1")))
+        )
+      )
+
+    type Waiting = IList[MachineNode]
+
+    type Patched_[a]  = Coproduct[BatchMachines.Ast, Orig, a]
+    type Stateful[a]  = State[Waiting, a]
+    type Patched_S[a] = Stateful[Patched_[a]]
+
+    val safeInterceptor: Orig ~> Patched_S = new (Orig ~> Patched_S) {
+      import Coproduct.{ leftc, rightc }
+
+      // FIXME: kind-projector syntax
+      def apply[α](fa: Orig[α]): Patched_S[α] = fa.run match {
+        case -\/(Machines.Start(node)) =>
+          State.get[Waiting] >>= {
+            case INil() =>
+              State.modify[Waiting](node :: _) >| leftc(BatchMachines.Noop())
+
+            case also =>
+              State.put[Waiting](IList.empty) >| leftc(
+                BatchMachines.Start(NonEmptyList.nel(node, also))
+              )
+          }
+
+        case other => rightc(Coproduct(other)).pure[State[Waiting, ?]]
+      }
+    }
+
+    // feels like it's something general... possibly with MonadTrans....
+    def Stateful[F[_], G[_]](
+      in: F ~> G
+    ): λ[α => Stateful[F[α]]] ~> λ[α => Stateful[G[α]]] = ???
+
+    type T_S[a] = Stateful[T[a]]
+    // why is scala not inferring this???!?!?!?
+    implicit def T_S: Monad[T_S] = ???
+
+    val interpreter: Patched_S ~> T_S = Stateful[Patched, T](or(B, or(M, D)))
+
+    program(Machines.liftF[Orig], Drone.liftF[Orig])
+      .mapSuspension(safeInterceptor)
+      .foldMap(interpreter)
+      .run(IList.empty) // for the Waiting interceptor
+      ._2
+      .run(S(IList.empty, IList.empty)) // for the app
+      .shouldBe(
+        S(
+          IList.empty,
+          IList(NonEmptyList(MachineNode("2"), MachineNode("1")))
+        )
+      )
+
   }
 
 }
