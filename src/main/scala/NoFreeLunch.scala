f0473138b3a69cad25b1cf5d6b13f75a8449b67a
Sam Halliday <sam.halliday@gmail.com>
2018-05-15 12:15:00 +0100

    moar minimised

--- src/main/scala/NoFreeLunch.scala
+++ src/main/scala/NoFreeLunch.scala
@@ -3,14 +3,11 @@
 
 package nofreelunch
 
-// Minimisation of issue encountered in drone-dynamic-agents' AlgebraSpec
+// We have an `Orig` AST and an `Improved` AST. We want to translate `Orig`
+// into `Improved` instructions, but need to carry some `State` to do so.
 //
-// We have an `Orig` AST and an `Improved` AST. We want to write `Orig ~>
-// Patched` where `Patched` is `Orig` plus `Improved`, wrapped with some
-// `State`.
-//
-// The problem is that to do this, we must introduce a fake `Noop` element. A
-// better solution would be to write `Orig ~> Free[Patched, ?]`. But I don't
+// The problem is that we must introduce a fake `Noop` element. A better
+// solution would be to write `Orig ~> Free[State[S, Improved], ?]`. But I don't
 // think it is possible to `State.sequence` or `Free.distribute`. Maybe it's
 // possible for these specific types?
 
@@ -22,38 +19,30 @@ final case class Old(i: Int) extends Orig[Unit]
 sealed abstract class Improved[A]
 final case class New(a: Int, b: Int) extends Improved[Unit]
 final case class Noop()              extends Improved[Unit]
-// we want to remove Noop() from the Improved AST
 
 object Main {
 
   type S            = Maybe[Int]
   type Extension[a] = State[S, Improved[a]]
-  type Patched[a]   = Coproduct[Extension, Orig, a]
 
   // requires Noop :-(
-  val hacky = λ[Orig ~> Patched] {
+  val hacky = λ[Orig ~> Extension] {
     case Old(i) =>
-      val extension: Extension[Unit] = State {
+      State {
         case Maybe.Just(s) => Maybe.empty   -> New(s, i)
         case Maybe.Empty() => Maybe.just(i) -> Noop()
       }
-      Coproduct.leftc(extension)
-
-    case other =>
-      Coproduct.rightc(other)
   }
 
   // this is the signature we want to write
-  val better = λ[Orig ~> Free[Patched, ?]] {
+  val better = λ[Orig ~> Free[Extension, ?]] {
     case Old(i) =>
       val extension: State[S, Free[Improved, Unit]] = State {
         case Maybe.Just(s) => Maybe.empty   -> Free.liftF(New(s, i))
         case Maybe.Empty() => Maybe.just(i) -> Free.pure(())
       }
-      ??? // :-(
 
-    case other =>
-      Free.liftF(Coproduct.right[Extension](other))
+      ??? // :-(
   }
 
 }
