a4847048b81a5556dcca7224e4d0a7fbe4b9db63
Sam Halliday <sam.halliday@gmail.com>
2018-05-30 23:06:41 +0100

    S should be visible

--- src/main/scala/freestate.scala
+++ src/main/scala/freestate.scala
@@ -6,13 +6,11 @@ package freestate
 import scalaz._, Scalaz._
 
 object FreeState {
-  sealed abstract class Ast[A] {
-    def widen: Ast[A] = this
-  }
-  final case class Get[S]()     extends Ast[S]
-  final case class Put[S](s: S) extends Ast[Unit]
+  sealed abstract class Ast[A, S]
+  final case class Get[S]()     extends Ast[S, S]
+  final case class Put[S](s: S) extends Ast[Unit, S]
 
-  def liftF[F[_], S](implicit I: Ast :<: F): MonadState[Free[F, ?], S] =
+  def liftF[F[_], S](implicit I: Ast[?, S] :<: F): MonadState[Free[F, ?], S] =
     new MonadState[Free[F, ?], S] {
       def point[A](a: =>A): Free[F, A] = Free.pure(a)
       def bind[A, B](fa: Free[F, A])(
@@ -20,9 +18,9 @@ object FreeState {
       ): Free[F, B] = fa.flatMap(f)
 
       def get: Free[F, S] =
-        Free.liftF(I.inj(Get[S]().widen))
+        Free.liftF(I.inj(Get[S]()))
       def put(s: S): Free[F, Unit] =
-        Free.liftF(I.inj(Put[S](s).widen))
+        Free.liftF(I.inj(Put[S](s)))
 
       def init: Free[F, S] = get
     }
