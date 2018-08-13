2500d3a17b19921c476a13f41e41ac4c08152d41
Sam Halliday <sam.halliday@gmail.com>
2018-08-13 23:33:12 +0100

    scalafix rules

--- src/main/scala/CordInterpolator.scala
+++ src/main/scala/CordInterpolator.scala
@@ -8,6 +8,7 @@ import Z._
 
 /** Cord is very inefficient in Scalaz 7.2, fixed in 7.3 */
 final class CordInterpolator(private val sc: StringContext) extends AnyVal {
+  // scalafix:off
   def show(args: CordInterpolator.Cords*): String = cord(args: _*).toString
   def cord(args: CordInterpolator.Cords*): Cord = {
     val strings = sc.parts.foldRight(IList.empty[Cord])(
@@ -16,13 +17,16 @@ final class CordInterpolator(private val sc: StringContext) extends AnyVal {
     val cords = args.foldRight(IList.empty[Cord])((a, acc) => a.cord :: acc)
     strings.interleave(cords).fold
   }
+  // scalafix:on
 }
 object CordInterpolator {
   // the interpolator takes Cords (not Cord) which allows us to restrict the
   // implicit materialisation of Cord (from Show instances) to this usecase.
   final class Cords private (val cord: Cord) extends AnyVal
   object Cords {
+    // scalafix:off
     implicit def trivial(c: Cord): Cords   = new Cords(c)
     implicit def mat[A: Show](a: A): Cords = new Cords(Show[A].show(a))
+    // scalafix:on
   }
 }
